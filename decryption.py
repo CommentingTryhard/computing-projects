cff9dd742850948eadca551b859c0b6957027118
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2024-02-21 08:01:07 +0800

    updated

--- decryption.py
+++ decryption.py
@@ -16,20 +16,26 @@ chooser = input("Would you like to encrypt or decrypt:(E/D) ")
 if (chooser == "E") or (chooser == "e"):
     for i in secret:
         seclist.append(i)
-    for letter in seclist:
-        if letter in "!@#$%^&*()<>?:,./;'[]\`~|_+":
+    print(seclist)
+    for index, value in enumerate(seclist):
+        print(value)
+        if value in "!@#$%^&*()<>?:,./;'[]\`~|_+":
             pass
         else:    
-            lend = ord(letter)+key
+            lend = ord(value)+key
             if lend < 123:
-                newletter = chr(ord(letter)+key)
-                letter = newletter
+                newletter = chr(ord(value)+key)
+                seclist.pop(index)
+                seclist.insert(index, newletter)
             if lend > 122:
-                orde = lend-27
-                newletter = chr(ord("a")+orde)
-                letter = newletter 
+                orde = lend-26
+                newletter = chr(orde)
+                seclist.pop(index)
+                seclist.insert(index, newletter)
     print(seclist)
-    print(*seclist)        
+    for i in ():
+        if i == " ":
+            seclist.replace(i,"")
 elif (chooser == "d") or (chooser == "d"):
     pass
 
