ecedc15ffb8b1a0f94dd3a15bbce33a608567e98
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2024-02-26 15:42:30 +0800

    26

---
+++ 26.2.2024.py
@@ -0,0 +1,44 @@
+#create an algorithnm to check the number of letter that appear in a setence.
+#The letter is requested as input.
+
+#create an algortihn that remove all instances of that word
+def lettercounter():
+    sentence = input("Please enter ur sentence: ")
+    letter = input("Please enter ur letter: ")
+    counter = 0
+    for i in sentence:
+        if letter == i:
+            counter+=1
+        else: pass
+    print(counter)
+
+def letterremover():
+    sentence = input("Please enter ur sentence: ")
+    letter = input("Please enter ur letter: ")
+    print(sentence.replace(letter, ""))
+
+def evenletterreplace():
+    sentence = input("Please enter ur sentence: ")
+    vowels = "AEIOUaeiou"
+    for index, value in enumerate(sentence):
+        if value in vowels:
+            if index%2==0:
+                sentence.replace(value, ":")
+                flag = True
+            else:
+                flag = False
+        else:
+            flag = False
+
+def splitter():
+    senlist = []
+    counter = 0
+    sentence = input("Please enter ur sentence: ")
+    for index, value in enumerate(sentence):
+        for i in sentence:
+            if i == value:
+                counter+=1
+
+def imcrazy():
+    a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z = 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    sentence
