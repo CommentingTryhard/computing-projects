b4b2e7895b5f790e8c88247aa947fc5fdf91765e
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2024-03-19 20:35:41 +0800

    rcc#2

--- scoreboard rcc.py
+++ scoreboard rcc.py
@@ -1,8 +1,6 @@
 def scoreboard():
-    data = database()
 
 
-def inputs():
     def database():
         #Guide to reading lists: L1 [0] = "N" [1] = "L" L2 [0] = "line" [1] = "timestamp" [2] = "teamid" [3] = "problemid" [4] = "inputid" [5] = "scored"
         database = [[5,5],[1,1,1,1,1,0],[2,2,1,1,1,1],[3,2,4,1,2,1],[4,3,2,1,2,0],[5,4,2,1,1,1]]
@@ -10,6 +8,12 @@ def inputs():
     def parameter():
         data = database()
         scorelist = []
+        for o in range(len(data)-1):
+            scorelist.append([])
         for i in range(5):
-            score = data[i,5]
-            scorelist.append(
+            score = data[i+1][5]
+            scorelist[i].append(score)
+            scorelist[i].append(data[i+1][1])
+
+
+
