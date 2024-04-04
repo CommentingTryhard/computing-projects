91d581631e0128f38a57a815749474a6904c53b1
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2024-04-04 16:07:52 +0800

    sfa

--- 3april.py
+++ 3april.py
@@ -2,27 +2,27 @@ studentcolour = []
 num_students = 10
 count=0
 flag = True
-def colourinput(studentcolour):
-    while flag and (count<10):
-        count+=1
-        colour = input("What is the student's favourite colour? ")
-        cinput = input("Please enter if you would like to enter another colour (Y/N):")
-        colour = colour.lower()
-        studentcolour.append(colour)
-        if cinput.lower() == "y":
-            flag = True
-        elif cinput.lower() == "n":
-            flag = False
-        else:
-            print("I do not understand your input.")
-    return studentcolour
-
-def search_list(studentcolour):
-    counter = 0
-    colour = input("Please input the colour you want to search for: ")
+counter = 0
+while flag and (count<10):
+    count+=1
+    colour = input("What is the student's favourite colour? ")
+    cinput = input("Please enter if you would like to enter another colour (Y/N):")
     colour = colour.lower()
-    for value in studentcolour:
-        if value == colour:
-            counter+=1
-    print(counter)
-    print(value)
+    studentcolour.append(colour)
+    if cinput.lower() == "y":
+        flag = True
+    elif cinput.lower() == "n":
+        flag = False
+    else:
+        print("I do not understand your input.")
+
+
+colour = input("Please input the colour you want to search for: ")
+colour = colour.lower()
+for value in studentcolour:
+    if value == colour:
+        counter+=1
+
+print(counter)
+print(value)
+print(studentcolour)
