27a197290bb8d6d4e92ec3b1aba6875d7e1e32e4
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2024-09-03 23:42:49 +0800

    new update

---
+++ newproj: network scanner/enc-v2.py
@@ -0,0 +1,119 @@
+import random as r
+
+seclist = []
+
+#encryption letters:
+"""
+a) key = len(1)
+b) key = len(any)
+"""
+
+secret = input("Enter your secret sentence: ")
+key = int(input("Please input your key: "))
+
+encrypted = ""
+chooser = input("Would you like to encrypt or decrypt:(E/D) ")
+if (chooser == "E") or (chooser == "e"):
+    for i in secret:
+        seclist.append(i)
+    for index, value in enumerate(seclist):
+        if value in "!@#$%^&*()<>?:,./;'[]\`~|_+":
+            pass
+        elif value.islower():  
+            lend = ord(value)+key
+            if lend < 123:
+                #break in
+                pissorde = orde*key
+                if pissorde%2!=0:
+                    orde = str(pissorde)+r.randint()
+                else:
+                    pass
+                for i in orde:
+                    if i%2==0:
+                        newletter = chr(orde[i]+orde[i+1])
+                        seclist.insert(index+1, newletter)
+                    else:
+                        pass
+                seclist.pop(index)
+            if lend > 122:
+                orde = lend-26
+                #break in
+                pissorde = orde*key
+                if pissorde%2!=0:
+                    orde = str(pissorde)+r.randint()
+                else:
+                    pass
+                for i in orde:
+                    if i%2==0:
+                        newletter = chr(orde[i]+orde[i+1])
+                        seclist.insert(index+1, newletter)
+                    else:
+                        pass
+                seclist.pop(index)
+        elif value.isupper():
+            lend = ord(value)+key
+            if lend < 91:
+                #break in
+                pissorde = orde*key
+                if pissorde%2!=0:
+                    orde = str(pissorde)+r.randint()
+                else:
+                    pass
+                for i in orde:
+                    if i%2==0:
+                        newletter = chr(orde[i]+orde[i+1])
+                        seclist.insert(index+1, newletter)
+                    else:
+                        pass
+                seclist.pop(index)
+            if lend > 90:
+                orde = lend+26
+                #break in
+                pissorde = orde*key
+                if pissorde%2!=0:
+                    orde = str(pissorde)+r.randint()
+                else:
+                    pass
+                for i in orde:
+                    if i%2==0:
+                        newletter = chr(orde[i]+orde[i+1])
+                        seclist.insert(index+1, newletter)
+                    else:
+                        pass
+                seclist.pop(index)
+
+    seclist = "".join(seclist)
+    print(seclist)
+elif (chooser == "D") or (chooser == "d"):
+    for i in secret:
+        seclist.append(i)
+    for index, value in enumerate(seclist):
+        if value in "!@#$%^&*()<>?:,./;'[]\`~|_+":
+            pass
+        elif value.islower():  
+            lend = ord(value)-key
+            if lend > 96:
+                newletter = chr(ord(value)-key)
+                seclist.pop(index)
+                seclist.insert(index, newletter)
+            if lend < 97:
+                orde = lend+26
+                newletter = chr(orde)
+                seclist.pop(index)
+                seclist.insert(index, newletter)
+        elif value.isupper():
+            lend = ord(value)-key
+            if lend < 91:
+                newletter = chr(ord(value)-key)
+                seclist.pop(index)
+                seclist.insert(index, newletter)
+            if lend > 90:
+                orde = lend-26
+                newletter = chr(orde)
+                seclist.pop(index)
+                seclist.insert(index, newletter)
+    seclist = "".join(seclist)
+    print(seclist)
+
+else:
+    print("Your letter was not able to be identified.")
