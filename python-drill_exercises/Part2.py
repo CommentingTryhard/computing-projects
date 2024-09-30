1ff3f3f41769c5522c854fb65fede8723e02ab6c
CommentingTryhard <88810104+CommentingTryhard@users.noreply.github.com>
2024-09-30 23:37:06 +0800

    p2

--- python-drill_exercises/Part2.py
+++ python-drill_exercises/Part2.py
@@ -35,3 +35,34 @@ def is_valid_password(password):
         else:
             password = input("Please input your password: ")
 
+def lenchecker(str1, str2):
+    if len(str1) == len(str2):
+        return True
+    else:
+        return False
+
+def are_anagrams(s1, s2):
+    s1list = []
+    for i in s1:
+        if not(i.isspace()):
+            s1list.append(i)
+    s2list = []
+    for i in s2:
+        if not(i.isspace()):
+            s2list.append(i)
+    s1list = s1list.sort()
+    s2list = s2list.sort()
+    if s1list == s2list:
+        return True
+    else:
+        return False
+
+def greater_than_ten_or_even(n):
+    if n <10:
+        if n&2==0:
+            return False
+        else:
+            return True
+    else:
+        return True
+
